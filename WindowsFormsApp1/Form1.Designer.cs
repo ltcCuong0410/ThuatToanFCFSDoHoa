2e0134be2d43fde35f8d8a31dd86320229504371
Cuong <phamkhanhly30012002@gmail.com>
2023-06-19 22:10:13 +0700

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtProcessName = new System.Windows.Forms.TextBox();
+            this.txtBurstTime = new System.Windows.Forms.TextBox();
+            this.txtAverageTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCompletionTime = new System.Windows.Forms.TextBox();
+            this.btnAddProcess = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridViewProcess = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcess)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "tên tiến trình";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "thời gian tiến trình";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 80);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "thời gian xuất hiện";
+            // 
+            // txtProcessName
+            // 
+            this.txtProcessName.Location = new System.Drawing.Point(128, 24);
+            this.txtProcessName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtProcessName.Name = "txtProcessName";
+            this.txtProcessName.Size = new System.Drawing.Size(76, 20);
+            this.txtProcessName.TabIndex = 1;
+            // 
+            // txtBurstTime
+            // 
+            this.txtBurstTime.Location = new System.Drawing.Point(128, 48);
+            this.txtBurstTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBurstTime.Name = "txtBurstTime";
+            this.txtBurstTime.Size = new System.Drawing.Size(76, 20);
+            this.txtBurstTime.TabIndex = 2;
+            // 
+            // txtAverageTime
+            // 
+            this.txtAverageTime.Location = new System.Drawing.Point(128, 75);
+            this.txtAverageTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAverageTime.Name = "txtAverageTime";
+            this.txtAverageTime.Size = new System.Drawing.Size(76, 20);
+            this.txtAverageTime.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(261, 28);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "thời gian hoàn thành";
+            // 
+            // txtCompletionTime
+            // 
+            this.txtCompletionTime.Location = new System.Drawing.Point(371, 28);
+            this.txtCompletionTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCompletionTime.Name = "txtCompletionTime";
+            this.txtCompletionTime.ReadOnly = true;
+            this.txtCompletionTime.Size = new System.Drawing.Size(84, 20);
+            this.txtCompletionTime.TabIndex = 10;
+            // 
+            // btnAddProcess
+            // 
+            this.btnAddProcess.BackColor = System.Drawing.Color.Thistle;
+            this.btnAddProcess.ForeColor = System.Drawing.Color.Red;
+            this.btnAddProcess.Location = new System.Drawing.Point(34, 117);
+            this.btnAddProcess.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddProcess.Name = "btnAddProcess";
+            this.btnAddProcess.Size = new System.Drawing.Size(56, 31);
+            this.btnAddProcess.TabIndex = 4;
+            this.btnAddProcess.Text = "thêm";
+            this.btnAddProcess.UseVisualStyleBackColor = false;
+            this.btnAddProcess.Click += new System.EventHandler(this.btnAddProcess_Click_1);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Thistle;
+            this.btnStart.ForeColor = System.Drawing.Color.Red;
+            this.btnStart.Location = new System.Drawing.Point(128, 117);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(56, 31);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "chạy";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.Color.Thistle;
+            this.BtnReset.ForeColor = System.Drawing.Color.Red;
+            this.BtnReset.Location = new System.Drawing.Point(228, 117);
+            this.BtnReset.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(56, 31);
+            this.BtnReset.TabIndex = 6;
+            this.BtnReset.Text = "reset";
+            this.BtnReset.UseVisualStyleBackColor = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Thistle;
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(330, 117);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 31);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "thoát";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridViewProcess
+            // 
+            this.dataGridViewProcess.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProcess.Location = new System.Drawing.Point(34, 162);
+            this.dataGridViewProcess.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewProcess.Name = "dataGridViewProcess";
+            this.dataGridViewProcess.RowHeadersWidth = 51;
+            this.dataGridViewProcess.RowTemplate.Height = 24;
+            this.dataGridViewProcess.Size = new System.Drawing.Size(524, 176);
+            this.dataGridViewProcess.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.dataGridViewProcess);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnAddProcess);
+            this.Controls.Add(this.txtCompletionTime);
+            this.Controls.Add(this.txtAverageTime);
+            this.Controls.Add(this.txtBurstTime);
+            this.Controls.Add(this.txtProcessName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProcess)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtProcessName;
+        private System.Windows.Forms.TextBox txtBurstTime;
+        private System.Windows.Forms.TextBox txtAverageTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCompletionTime;
+        private System.Windows.Forms.Button btnAddProcess;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridViewProcess;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Queue<Process> processQueue; // Hàng đợi tiến trình
+        private List<Process> completedProcesses; // Danh sách tiến trình đã hoàn thành
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            processQueue = new Queue<Process>();
+            completedProcesses = new List<Process>();
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Tạo các cột trong DataGridView
+            DataGridViewTextBoxColumn colProcessName = new DataGridViewTextBoxColumn();
+            colProcessName.HeaderText = "Tên tiến trình";
+            colProcessName.Name = "colProcessName";
+
+            DataGridViewTextBoxColumn colBurstTime = new DataGridViewTextBoxColumn();
+            colBurstTime.HeaderText = "Thời gian thực thi";
+            colBurstTime.Name = "colBurstTime";
+
+            DataGridViewTextBoxColumn colStatus = new DataGridViewTextBoxColumn();
+            colStatus.HeaderText = "Trạng thái";
+            colStatus.Name = "colStatus";
+
+            DataGridViewTextBoxColumn colTurnaroundTime = new DataGridViewTextBoxColumn();
+            colTurnaroundTime.HeaderText = "Thời gian tồn tại";
+            colTurnaroundTime.Name = "colTurnaroundTime";
+
+            DataGridViewTextBoxColumn colTurnaroundTimeAvg = new DataGridViewTextBoxColumn();
+            colTurnaroundTimeAvg.HeaderText = "Thời gian tồn tại trung bình";
+            colTurnaroundTimeAvg.Name = "colTurnaroundTimeAvg";
+
+            // Thêm các cột vào DataGridView
+            dataGridViewProcess.Columns.Add(colProcessName);
+            dataGridViewProcess.Columns.Add(colBurstTime);
+            dataGridViewProcess.Columns.Add(colStatus);
+            dataGridViewProcess.Columns.Add(colTurnaroundTime);
+            dataGridViewProcess.Columns.Add(colTurnaroundTimeAvg);
+
+            // Đăng ký sự kiện CellFormatting
+            dataGridViewProcess.CellFormatting += DataGridViewProcess_CellFormatting;
+        }
+
+        private void DataGridViewProcess_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            // Kiểm tra cột trạng thái
+            if (dataGridViewProcess.Columns[e.ColumnIndex].Name == "colStatus")
+            {
+                // Lấy giá trị của ô cột trạng thái
+                string status = e.Value?.ToString();
+
+                // Thiết lập màu nền dựa trên giá trị trạng thái
+                if (status == "Đang chạy")
+                {
+                    e.CellStyle.BackColor = Color.Yellow;
+                }
+                else if (status == "Hoàn thành")
+                {
+                    e.CellStyle.BackColor = Color.Green;
+                }
+                else if (status == "Chờ chạy")
+                {
+                    e.CellStyle.BackColor = Color.Red;
+                }
+                else
+                {
+                    e.CellStyle.BackColor = Color.White;
+                }
+            }
+        }
+
+
+
+
+
+        private void CapNhatTrangThaiTienTrinh(string processName, string status)
+        {
+            foreach (DataGridViewRow row in dataGridViewProcess.Rows)
+            {
+                if (row.Cells["colProcessName"].Value?.ToString() == processName)
+                {
+                    int statusColumnIndex = dataGridViewProcess.Columns["colStatus"].Index;
+
+                    // Kiểm tra giá trị chỉ số cột
+                    if (statusColumnIndex >= 0 && statusColumnIndex < dataGridViewProcess.Columns.Count)
+                    {
+                        row.Cells[statusColumnIndex].Value = status;
+                    }
+                    else
+                    {
+                        // Xử lý lỗi ở đây (nếu có)
+                    }
+
+                    break;
+                }
+            }
+        }
+        int completionTime = 0;
+        private void CapNhatDanhSachTienTrinh()
+        {
+             // Tổng thời gian hoàn thành
+            
+
+
+
+            foreach (Process process in completedProcesses)
+            {
+                foreach (DataGridViewRow row in dataGridViewProcess.Rows)
+                {
+                    if (row.Cells["colProcessName"].Value?.ToString() == process.Name)
+                    {
+                        int turnaroundTimeColumnIndex = dataGridViewProcess.Columns["colTurnaroundTime"].Index;
+
+                        // Kiểm tra giá trị chỉ số cột
+                        if (turnaroundTimeColumnIndex >= 0 && turnaroundTimeColumnIndex < dataGridViewProcess.Columns.Count)
+                        {
+                            row.Cells[turnaroundTimeColumnIndex].Value = process.TurnaroundTime.ToString();
+                        }
+                        else
+                        {
+                            // Xử lý lỗi ở đây (nếu có)
+                        }
+                        
+                        
+
+                        break;
+                    }
+                    
+                }
+                completionTime = Math.Max(completionTime, process.TurnaroundTime);
+            }
+            
+            
+            // Tính thời gian tồn tại trung bình mỗi tiến trình và hiển thị trên DataGridView
+            double averageTurnaroundTime = completedProcesses.Average(p => p.TurnaroundTime);
+
+            int averageTurnaroundTimeColumnIndex = dataGridViewProcess.Columns["colTurnaroundTimeAvg"].Index;
+
+            // Kiểm tra giá trị chỉ số cột
+            if (averageTurnaroundTimeColumnIndex >= 0 && averageTurnaroundTimeColumnIndex < dataGridViewProcess.Columns.Count)
+            {
+                foreach (DataGridViewRow row in dataGridViewProcess.Rows)
+                {
+                    row.Cells[averageTurnaroundTimeColumnIndex].Value = averageTurnaroundTime.ToString("0.00");
+                }
+            }
+            else
+            {
+                // Xử lý lỗi ở đây (nếu có)
+            }
+            
+        }
+
+        private void btnAddProcess_Click_1(object sender, EventArgs e)
+        {
+            // Kiểm tra dữ liệu đầu vào
+            if (string.IsNullOrWhiteSpace(txtProcessName.Text) || string.IsNullOrWhiteSpace(txtBurstTime.Text))
+            {
+                MessageBox.Show("Vui lòng nhập tên tiến trình và thời gian thực thi.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string processName = txtProcessName.Text;
+            int burstTime = Convert.ToInt32(txtBurstTime.Text);
+
+            // Tạo một tiến trình mới
+            Process process = new Process(processName, burstTime);
+
+            // Thêm tiến trình vào hàng đợi
+            processQueue.Enqueue(process);
+
+            // Thêm tiến trình vào DataGridView
+            dataGridViewProcess.Rows.Add(processName, burstTime, "", "");
+
+            // Xóa các trường đầu vào
+            txtProcessName.Text = "";
+            txtBurstTime.Text = "";
+            txtAverageTime.Text = "";
+        }
+
+        private async void btnStart_Click_1(object sender, EventArgs e)
+        {
+            int totalBurstTime = 0; // Tổng thời gian thực thi của các tiến trình
+            int totalProcesses = dataGridViewProcess.Rows.Count; // Tổng số tiến trình
+            
+            while (processQueue.Count > 0)
+            {
+                Process currentProcess = processQueue.Dequeue();
+
+                // Cập nhật trạng thái tiến trình
+                CapNhatTrangThaiTienTrinh(currentProcess.Name, "Đang chạy");
+
+                // Giả lập thực thi tiến trình
+                await Task.Delay(currentProcess.BurstTime * 1000);
+
+                // Cập nhật trạng thái tiến trình
+                CapNhatTrangThaiTienTrinh(currentProcess.Name, "Hoàn thành");
+
+                // Tính thời gian tồn tại của tiến trình
+                currentProcess.TurnaroundTime = totalBurstTime + currentProcess.BurstTime;
+
+                // Thêm tiến trình đã hoàn thành vào danh sách
+                completedProcesses.Add(currentProcess);
+
+                // Cập nhật danh sách tiến trình
+                CapNhatDanhSachTienTrinh();
+                // Hiển thị thời gian hoàn thành trên TextBox
+                
+                totalBurstTime += currentProcess.BurstTime;
+            }
+            // Tính thời gian hoàn thành
+            
+
+            txtCompletionTime.Text = completionTime.ToString();
+
+
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            processQueue.Clear();
+            completedProcesses.Clear();
+            dataGridViewProcess.Rows.Clear();
+            txtCompletionTime.Text = "";
+            txtAverageTime.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    // Lớp Process đại diện cho một tiến trình
+    public class Process
+    {
+        public string Name { get; set; } // Tên tiến trình
+        public int BurstTime { get; set; } // Thời gian thực thi
+        public int TurnaroundTime { get; set; } // Thời gian tồn tại
+
+        public Process(string name, int burstTime)
+        {
+            Name = name;
+            BurstTime = burstTime;
+            TurnaroundTime = 0;
+        }
+    }
+}
